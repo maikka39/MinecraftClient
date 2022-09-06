12c5f1c5040fd96b435ea4a8d44157068ac0b4ee
Maik de Kruif <maik@maik.dev>
2022-09-06 18:14:32 +0200

    Cleanup EventUtils a bit

--- src/main/kotlin/event/EventUtils.kt
+++ src/main/kotlin/event/EventUtils.kt
@@ -1,8 +1,33 @@
 package event
 
 import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
 import kotlin.reflect.full.*
 import kotlin.reflect.jvm.isAccessible
+
+class InvalidFunctionHeaderForEventHandler(func: KFunction<*>, obj: Any) :
+    Exception("Invalid parameter count for EventHandler \"${func.name}\" in class \"${obj::class.simpleName}\". Only () and (event: <T: Event>), where T is of the same type as event in in EventHandler(event), are accepted.")
+
+private fun getEventListener(ann: EventHandler, func: KFunction<*>, obj: Any): EventListener<Event> {
+    return when (func.parameters.count()) {
+        1 -> EventListener { func.call(obj) }
+        2 ->
+            if (func.parameters[1] == func.extensionReceiverParameter)
+                EventListener { func.call(obj, obj) }
+            else if (func.parameters[1].type == ann.event.starProjectedType)
+                EventListener { func.call(obj, it) }
+            else
+                throw InvalidFunctionHeaderForEventHandler(func, obj)
+        3 ->
+            if (func.extensionReceiverParameter == null)
+                throw InvalidFunctionHeaderForEventHandler(func, obj)
+            else if (func.parameters[2].type != ann.event.starProjectedType)
+                throw InvalidFunctionHeaderForEventHandler(func, obj)
+            else
+                return EventListener { func.call(obj, obj, it) }
+        else -> throw InvalidFunctionHeaderForEventHandler(func, obj)
+    }
+}
 
 @Suppress("UNCHECKED_CAST")
 fun registerEventsInObject(obj: Any) {
@@ -12,20 +37,12 @@ fun registerEventsInObject(obj: Any) {
         val ann = func.annotations.firstOrNull { it.annotationClass == EventHandler::class } as EventHandler?
             ?: return@forEach
 
-        if (!ann.event.isSubclassOf(Event::class)) throw Exception("Event of \"${func.name}\" in class \"${obj::class.simpleName}\" must be a subtype of Event")
+        if (!ann.event.isSubclassOf(Event::class))
+            throw Exception("Event of \"${func.name}\" in class \"${obj::class.simpleName}\" must be a subtype of Event")
 
         func.isAccessible = true
 
-        val eventListener: EventListener<Event> = if (func.parameters.count() == 1)
-            EventListener { func.call(obj) }
-        else if (func.parameters.count() == 2 && func.parameters[1].type == ann.event.starProjectedType)
-            EventListener { func.call(obj, it) }
-        else if (func.parameters.count() == 2 && func.extensionReceiverParameter != null)
-            EventListener { func.call(obj, obj) }
-        else if (func.parameters.count() == 2 && func.extensionReceiverParameter != null && func.parameters[2].type == ann.event.starProjectedType)
-            EventListener { func.call(obj, obj, it) }
-        else
-            throw Exception("Invalid parameter count for EventHandler \"${func.name}\" in class \"${obj::class.simpleName}\". Only () and (event: <T: Event>), where T is of the same type as event in in EventHandler(event), are accepted.")
+        val eventListener: EventListener<Event> = getEventListener(ann, func, obj)
 
         EventManager.register(ann.event as KClass<out Event>, eventListener)
     }
